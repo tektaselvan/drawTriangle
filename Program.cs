0157e2bc1ee0b20c678d06abe4835b67df60ccb8
Elvan Tektaş <tektaselvan.27@gmail.com>
2023-08-19 18:04:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace drawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = 0;
+            Console.Write("Uzunluk ölçüsü giriniz: ");
+            lenght = int.Parse(Console.ReadLine());
+
+            int i, j, k;
+            for (i = 1; i <= lenght; i++)
+            {
+                for (j = 1; j <= i - lenght; j++)
+                {
+                    //Console.WriteLine("*");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
